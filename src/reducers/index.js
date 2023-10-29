51b625b9c2196a235c23bbdd87a8d88437bec7a6
Tobias Legenden <tobiaslegenden@gmail.com>
2023-10-29 17:41:56 +0100

    init

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import agGridReducer from './agGridReducers';
+import filterReducer from './filterReducers';
 
 const rootReducer = combineReducers({
   agGrid: agGridReducer,
+  filterOptions: filterReducer
 });
 
 export default rootReducer;
